a2b41c9acaeb889cee9f21c8f7fa02d90ff7989c
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-05-11 03:14:04 +0530

    corrected code

--- Dynamic_Programming/SubsetSum.cpp
+++ Dynamic_Programming/SubsetSum.cpp
@@ -9,14 +9,16 @@ int SubsetSum_1(vector<int>& nums, int target){
 	// dp[i][j] = do we have elements in arr[0...i] such that they add up to j?
 	
 
-	dp[0][0] = true;
+	for(int i=0; i<=n; i++){
+		dp[i][0] = true;
+	}
 
 	for(int j=1; j<=target; j++){
 		for(int i=1; i<=n; i++){
 			if( j - nums[i-1] >= 0 ){
-				dp[i][j] = dp[i][j] or dp[i-1][j-nums[i-1]];
+				dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]];
 			}else{
-				dp[i][j] = dp[i][j] or dp[i-1][j];
+				dp[i][j] = dp[i-1][j];
 			}
 		}
 	}
@@ -25,5 +27,5 @@ int SubsetSum_1(vector<int>& nums, int target){
 }
 int main(){
 	vector<int> arr{3,2,1,2,3,4,6,11,2};
-	cout<<SubsetSum_1(arr,9);
+	cout<<SubsetSum_1(arr,88);
 }
