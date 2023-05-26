98501161b726cf19456e661e947fda9be863cb95
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-05-26 23:24:05 +0530

    corrected

--- Dynamic_Programming/LongestSubsequence.cpp
+++ Dynamic_Programming/LongestSubsequence.cpp
@@ -45,8 +45,38 @@ int longestIncreasingSubsequence(vector<int>& nums, int memo){
   
   return *max_element(dp.begin(),dp.end());
 }
+
+vector<int> LIS(vector<int>& nums){
+  int n = nums.size();
+  vector<int> dp(n,1);
+  vector<int> p(n,-1);
+
+  for(int i=1; i<n; i++){
+    for(int j=0; j<i; j++){
+      if( nums[j] < nums[i] && dp[j] + 1 > dp[i] ){
+        dp[i] = dp[j] + 1;
+        p[i] = j;
+      }
+    }
+  }
+
+  int max_idx = max_element(dp.begin(),dp.end()) - dp.begin();
+  
+  vector<int> ans;
+  int i = max_idx;
+  while( i >= 0 ){
+    ans.push_back(nums[i]);
+    i = p[i];
+  }
+  return ans;
+}
 int main(){
   vector<int> nums{2,1,1,4,2,5,3,6,10,8,9};
   cout<<longestIncreasingSubsequence(nums,1)<<endl;
   cout<<longestIncreasingSubsequence(nums, 0)<<endl;
+  vector<int> res = LIS(nums);
+  for(int x : res){
+    cout<<x<<" ";
+  }
+  cout<<endl;
 }
---
+++ Dynamic_Programming/to-do-LIS.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+/*
+* Link : https://leetcode.com/problems/longest-increasing-subsequence-ii/description/
+*/
+int main(){
+
+}
