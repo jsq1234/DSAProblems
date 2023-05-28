cc6755b6174ad72238aceb0aa058153b407c7446
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-05-28 15:16:22 +0530

    stone_game 4 added

--- Dynamic_Programming/stone_game4.cpp
+++ Dynamic_Programming/stone_game4.cpp
@@ -1,31 +1,42 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#include <string.h>
+
 using namespace std;
 
+#define ALICE 1
+#define BOB 0
 class Solution {
 public:
-    int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
-        int n = aliceValues.size();
-        priority_queue<pair<int,int>> pq;
-        for(int i=0; i<n; i++){
-            pq.push({ aliceValues[i] + bobValues[i], i });
-        }
-
-        int alice = 0, bob = 0, turn = 0;
-        while(!pq.empty()){
-            if(!turn){
-                alice += aliceValues[pq.top().second];
-                pq.pop();
-                
-            }else{
-                bob += bobValues[pq.top().second];
-                pq.pop();
-            
-            }
-            turn = !turn;
-        }
-        
-        if(alice == bob) return 0;
-        return alice > bob ? 1 : -1;
-
+    int dp[100001][2];
+    bool winnerSquareGame(int n) {
+        int rt = sqrt(n);
+        if( rt*rt == n ) return true;
+        memset(dp,-1,sizeof(dp));
+        return solve(n,ALICE);
     }
+
+    bool solve(int n, int turn){
+        if( n <= 0 ) return false;
+        if( n == 1 ) return true;
+        if( dp[n][turn] != -1 ) return dp[n][turn];
+        int rt = sqrt(n);
+        if( rt*rt == n ) return dp[n][turn] = true;
+        int i = 1, j = 2;
+        int ans = false;
+        while( i <= n ){
+            bool opponent = solve(n - i,!turn);
+            ans = ans || (!opponent);
+            if(ans){
+                return dp[n][turn] = true;
+            }
+            i = j*j;
+            j++;
+        }
+        return dp[n][turn] = ans;
+    }
+    
 };
