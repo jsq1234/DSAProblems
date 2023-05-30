51486ee89cb0b4dfe626b0a0ae3ad536e9142119
= <=>
2023-05-30 17:51:41 +0530

    another file, delete is later on

---
+++ Dynamic_Programming/leetcode.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  for (int i = 0; i < 10; i++) {
+    cout << "Hello World\n";
+  }
+}
