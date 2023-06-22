3a9d82b236e7e2005751d0b2fbf1ab27b6dbc2a3
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-06-23 00:53:15 +0530

    added

--- Dynamic_Programming/Make_Array_Strictly_Increasing.cpp
+++ Dynamic_Programming/Make_Array_Strictly_Increasing.cpp
@@ -1,52 +1,40 @@
-#include <vector>
-#include <iostream>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
-        int n = arr1.size();
-        int m = arr2.size();
-        sort(arr2.begin(),arr2.end());
-        return solve(arr1,arr2,0,0,-1);
-    }
 
-    int solve(vector<int>& arr1, vector<int>& arr2 , int i, int j, int prev){
-        
-        if( i >= arr1.size() ){
+    int solve(vector<int>& arr1, vector<int>& arr2, 
+                vector<unordered_map<int,int>>& dp, int i, int prev){
+        if( i >= arr1.size() ) {
             return 0;
-        }else{
-            if( j >= arr2.size() ){
-                return -1;
-            }
         }
-        // get the index which is atleast greater than prev
-        int pos = lower_bound(arr2.begin()+j, arr2.end(), prev + 1) - arr2.begin();
 
-        if( prev < arr1[i]){
-            int pick = solve(arr1,arr2,i+1,pos+1,arr2[pos]);
-            int notpick = solve(arr1,arr2,i+1,pos+1,arr1[i]);
-            if( pick == -1 || notpick == -1 ){
-                return -1;
-            }
-            return min(1 + pick, notpick);
+        if( dp[i].find(prev) != dp[i].end()){
+            return dp[i][prev];
         }
-        
-        int r = solve(arr1,arr2,i+1,pos+1,arr2[pos]);
-        
-        return r == -1 ? -1 : 1 + r;
-        
 
+        int take = INT_MAX, not_take = INT_MAX;
+        if( prev < arr1[i] ){
+            not_take = solve(arr1,arr2,dp,i+1,arr1[i]);
+        }
+
+        int idx = upper_bound(arr2.begin(), arr2.end(), prev) - arr2.begin();
+
+        if( idx < arr2.size() ){
+            take = solve(arr1,arr2,dp,i+1,arr2[idx]);
+        }
+
+        if( take == INT_MAX ){
+            return dp[i][prev] = not_take;
+        }
+
+        return dp[i][prev] = min(take+1,not_take);
+    }
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        sort(arr2.begin(),arr2.end());
+        vector<unordered_map<int,int>> dp(2001);
+        int val = solve(arr1,arr2,dp,0,-1);
+        return val == INT_MAX ? -1 : val;
     }
 };
-
-
-int main(){
-    Solution sol;
-    vector<int> arr1{1,5,3,6,7};
-    vector<int> arr2{4,3,1};
-
-    cout<<sol.makeArrayIncreasing(arr1, arr2)<<endl;
-}
