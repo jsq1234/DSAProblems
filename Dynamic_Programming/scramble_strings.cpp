bbb1d8cde5f3a3987d6900386d767986874071a7
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-09 18:17:40 +0530

    added scramble strings

--- Dynamic_Programming/scramble_strings.cpp
+++ Dynamic_Programming/scramble_strings.cpp
@@ -1,18 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
+    int dp[31][31][31][31];
     bool isScramble(string& s1, string& s2) {
         int n = s1.length();
-        return false;
+        int m = s2.length();
+
+        memset(dp,-1,sizeof(dp));
+
+        return solve(s1,s2,0,n-1,0,m-1);
     }
     
-    /* 
-     * solve(s1,s2,i,j) -> whether s1[i..j] is scramble string of s2[i..j]
-     * 
-     * /
     bool solve(string& s1, string& s2, int i, int j, int l, int m){
+        if( i == j && l == m ) return s1[i] == s2[l];
+        if( i > j || l > m ) return false;
+
+        if( dp[i][j][l][m] != -1 ) return dp[i][j][l][m];
+
+        int ans = false;
         
+        for(int k=i; k<j; k++){
+            int temp = false, temp2 = false;
+            temp = solve(s1,s2,i,k,l,l+k-i) && solve(s1,s2,k+1,j,l+k-i+1,m);
+            temp2 = solve(s1,s2,k+1,j,l,l+j-k-1) && solve(s1,s2,i,k,l+j-k,m);
+            if( temp || temp2 ){
+                return dp[i][j][l][m] = true;
+            }
+        }
+        return dp[i][j][l][m] = false;
     }
+
 };
