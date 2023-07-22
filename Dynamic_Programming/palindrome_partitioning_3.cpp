09947a0081b0d27d1644ba8e0f993f914204e61d
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-22 21:16:39 +0530

    updated

--- Dynamic_Programming/palindrome_partitioning_3.cpp
+++ Dynamic_Programming/palindrome_partitioning_3.cpp
@@ -3,19 +3,35 @@ using namespace std;
 
 class Solution {
 public:
-    int drome[101][101];
+    int dr[101][101];
     int palindromePartition(string& s, int k) {
-        memset(drome,0,sizeof(drome));
+        memset(dr,0,sizeof(dr));
         int n = s.length();
         
         for(int i=0; i<n; i++){
-            drome[i][i] = 1;
-            if( i < n-1 && s[i] == s[i+1] ) drome[i][i+1] = 1;
+            dr[i][i] = 1;
+            if( i < n-1 && s[i] == s[i+1] ) dr[i][i+1] = 1;
         }
 
-        for(int len=3; len<=n-len; len++){
+        for(int len=3; len<=n; len++){
+            for(int i=0; i<= n - len; i++){
+                int j = i + len - 1;
+                if(s[i] == s[j] && dr[i+1][j-1]){
+                    dr[i][j] = 1;
+                }
+            }
+        }
+
+        // dr[i][j] -> s[i..j] is palindrome or not
+    }    
+
+    int solve(string& s, int i){
+        int n = s.length();
+        if( i >= n ) return 0;
+        
+        for(int k=i; k<n; k++){
             
         }
-
-    }    
+        
+    }
 };
