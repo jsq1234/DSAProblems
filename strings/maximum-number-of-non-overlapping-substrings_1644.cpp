3e6e80ff3c4eabf1fa1f8ac2daf7a58464f12f79
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-30 18:04:29 +0530

    completed a problem

--- strings/maximum-number-of-non-overlapping-substrings_1644.cpp
+++ strings/maximum-number-of-non-overlapping-substrings_1644.cpp
@@ -11,36 +11,50 @@ using namespace std;
 
 class Solution {
 public:
-    vector<string> maxNumOfSubstrings(string& s) {
+    vector<string> maxNumOfSubstrings(string& s)
+    {
         int n = s.length();
 
-        unordered_map<char,pair<int,int>> mp;
+        unordered_map<char, pair<int, int>> mp;
 
-        for(int i=0; i<n; i++){
-            if(mp.find(s[i]) != mp.end()){
+        for (int i = 0; i < n; i++) {
+            if (mp.find(s[i]) == mp.end()) {
                 mp[s[i]].first = i;
                 mp[s[i]].second = i;
-            }else{
+            } else {
                 mp[s[i]].second = i;
             }
         }
 
-        vector<string> ans;
+        vector<vector<int>> arr;
+        for (auto& p : mp) {
+            int min_idx = p.second.first;
+            int max_idx = p.second.second;
 
-        for(int i=0; i<n; i++){
-            int first_index = mp[s[i]].first;
-            if( first_index < i ){
-                continue;
+            for (int k = min_idx; k <= max_idx; k++) {
+                min_idx = min(min_idx, mp[s[k]].first);
+                max_idx = max(max_idx, mp[s[k]].second);
             }
-            int last_index = mp[s[i]].second;
+
+            if (min_idx == p.second.first) {
+                arr.push_back({ min_idx, max_idx });
+            }
         }
+
+        sort(arr.begin(), arr.end(), [](vector<int>& v1, vector<int>& v2) {
+            return v1[1] < v2[1];
+        });
+
+        vector<string> ans;
+        int prev = -1;
+        for(auto& v : arr){
+            if( prev < v[0] ){
+                ans.push_back(s.substr(v[0],v[1]-v[0]+1));
+                prev = v[1];
+            }
+        }
+
+        sort(ans.begin(),ans.end(),[](string& s1, string& s2){ return s1.length() < s2.length(); });
+        return ans;
     }
 };
-
-int main(){
-    Solution sol;
-    string s = "adefaddaccc";
-    string s2 = "abbabcd";
-    sol.maxNumOfSubstrings(s);
-    sol.maxNumOfSubstrings(s2);
-}
