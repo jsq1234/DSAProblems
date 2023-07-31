b1f4da1066d3604a8f63f35dffa650a95497b7ca
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-07-31 12:20:49 +0530

    completed strange printer

--- Dynamic_Programming/strange-printer_664.cpp
+++ Dynamic_Programming/strange-printer_664.cpp
@@ -9,26 +9,28 @@ using namespace std;
 
 class Solution {
 public:
-    int strangePrinter(string& s) { 
-        return solve(s,0,s.length());
+    int dp[101][101];
+    int strangePrinter(string& s)
+    {
+        memset(dp, -1, sizeof(dp));
+        return solve(s, 0, s.length() - 1);
     }
 
-    int solve(string& s, int i, int j){
-        if( i >= j ) return 0;
-        char ch = s[i];
-        int count = 1;
-        for(int k=i; k<j;){
-            if(s[k] != ch){
-               int r = k;
-               while(r<j && s[r] != ch) r++;
-               count += solve(s,k,r);
-               k = r;
-               continue;
+    int solve(string& s, int i, int j)
+    {
+        if (i > j)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int result = solve(s, i, j - 1) + 1;
+
+        for (int k = i; k < j; k++) {
+            if (s[k] == s[j]) {
+                result = min(result, solve(s, i, k) + solve(s, k + 1, j - 1));
             }
-            k++;
         }
-        return count;
+        return dp[i][j] = result;
     }
 };
-
-
