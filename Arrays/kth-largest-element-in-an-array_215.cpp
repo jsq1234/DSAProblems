350e4422ffc9af10e8af1566aeff129f35aff07d
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-08-08 23:01:15 +0530

    added new question

---
+++ Arrays/kth-largest-element-in-an-array_215.cpp
@@ -0,0 +1,26 @@
+/*
+
+[3,2,1,5,6,4]
+2;
+
+[3,2,3,1,2,4,5,5,6]
+4;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int i=0; i<k; i++) pq.push(nums[i]);
+        for(int i=k; i<nums.size(); i++){
+            if(nums[i] > pq.top()){
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+
+        return pq.top();
+    }
+};
