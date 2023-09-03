59d46f34f18c618c61f5e8d897a6da89b54dec7f
Manan Prakash Singh <mananprakash3078@gmail.com>
2023-09-03 18:52:09 +0530

    removed txt files

---
+++ Dynamic_Programming/count-k-subsequences-of-a-string-with-maximum-beauty_3057.cpp
@@ -0,0 +1,15 @@
+/*
+
+"bcca"
+2;
+
+"abbcd"
+4;
+
+*/
+class Solution {
+public:
+    int countKSubsequencesWithMaxBeauty(string s, int k) {
+        
+    }
+};
---
+++ Dynamic_Programming/maximum-product-of-the-length-of-two-palindromic-subsequences_2130.cpp
@@ -0,0 +1,20 @@
+/*
+
+"leetcodecom";
+
+"bb";
+
+"accbcaxxcxx";
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(string& s) {
+        int n = s.length();
+        unsigned int mask = (1<<n) - 1;
+        
+    }
+};
